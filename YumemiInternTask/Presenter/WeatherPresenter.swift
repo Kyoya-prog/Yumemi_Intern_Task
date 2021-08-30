1e8084316e9edc4cdb0b87304aa66ce947749714
Kyoya-prog <kyoyaprog@gmail.com>
2021-08-30 14:40:49 +0900

    文字列からアイコンを取得する処理を追加

--- YumemiInternTask/Presenter/WeatherPresenter.swift
+++ YumemiInternTask/Presenter/WeatherPresenter.swift
@@ -15,7 +15,27 @@ class WeatherPresenter:WeatherPresentation,WeatherPresentationOutput{
     }
     
     func outputWeather(weatherString:String) {
-        view.showWeatherImage(image: UIImage())
+        let image = decideWeatherIcon(weatherString: weatherString)
+        view.showWeatherImage(image: image)
+    }
+    
+    private func decideWeatherIcon(weatherString:String)->UIImage {
+        var image:UIImage?
+        switch weatherString {
+        case "sunny":
+            image = UIImage(named: "sunny")
+        case "cloudy":
+            image = UIImage(named: "cloudy")
+        case "rainy":
+            image = UIImage(named: "rainy")
+        default:
+            image = nil
+        }
+        guard let image = image else {
+            fatalError("unexpected weather string was returned")
+        }
+        
+        return image
     }
 
 }
