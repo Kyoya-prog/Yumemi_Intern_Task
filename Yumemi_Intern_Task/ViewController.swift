9e124fcc5cff2b47a513c9212f530a96fd3a1db1
Kyoya-prog <kyoyaprog@gmail.com>
2021-08-30 12:14:32 +0900

    SubViewを実装

--- Yumemi_Intern_Task/ViewController.swift
+++ Yumemi_Intern_Task/ViewController.swift
@@ -1,15 +1,51 @@
-//
-//  ViewController.swift
-//  Yumemi_Intern_Task
-//
-//  Created by 松山 響也 on 2021/08/30.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpSubViews()
+    }
+
+    private let weatherImageView = UIImageView()
+
+    private let blueLabel = UILabel()
+
+    private let redLabel = UILabel()
+
+    private let closeButton = UIButton()
+
+    private let reloadButton = UIButton()
+
+    private let centerView = UIView()
+
+    private func setUpSubViews() {
+        centerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(centerView)
+
+        weatherImageView.translatesAutoresizingMaskIntoConstraints = false
+        weatherImageView.backgroundColor = .black
+        centerView.addSubview(weatherImageView)
+
+        blueLabel.translatesAutoresizingMaskIntoConstraints = false
+        blueLabel.text = "blue"
+        blueLabel.textColor = .blue
+        blueLabel.textAlignment = .center
+        centerView.addSubview(blueLabel)
+
+        redLabel.translatesAutoresizingMaskIntoConstraints = false
+        redLabel.text = "red"
+        redLabel.textColor = .red
+        redLabel.textAlignment = .center
+        centerView.addSubview(redLabel)
+
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.setTitle("close", for: .normal)
+        closeButton.setTitleColor(Color.buttonColor1, for: .normal)
+        view.addSubview(closeButton)
+
+        reloadButton.translatesAutoresizingMaskIntoConstraints = false
+        reloadButton.setTitle("reload", for: .normal)
+        reloadButton.setTitleColor(Color.buttonColor1, for: .normal)
+        view.addSubview(reloadButton)
     }
 }
