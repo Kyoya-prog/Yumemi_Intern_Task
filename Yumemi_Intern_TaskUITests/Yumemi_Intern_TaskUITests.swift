5d39d4e38d5f7ea8d660d495286301c2817b6a35
Kyoya-prog <kyoyaprog@gmail.com>
2021-08-30 11:47:32 +0900

    swiftlintでクラス名が不適切なことによるエラーが発生したので修正

--- Yumemi_Intern_TaskUITests/Yumemi_Intern_TaskUITests.swift
+++ Yumemi_Intern_TaskUITests/Yumemi_Intern_TaskUITests.swift
@@ -7,8 +7,7 @@
 
 import XCTest
 
-class Yumemi_Intern_TaskUITests: XCTestCase {
-
+class YumemiInternTaskUITests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
