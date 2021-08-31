5e26af66eea08eef55b500f1f38a7d00aa3540d5
Kyoya-prog <kyoyaprog@gmail.com>
2021-08-31 14:30:44 +0900

    ファイルを整理

--- YumemiInternTask/Data/WeatherError.swift
+++ YumemiInternTask/Data/WeatherError.swift
@@ -1,19 +1,7 @@
 import Foundation
 import UIKit
-import enum YumemiWeather.YumemiWeatherError
 
 enum WeatherError {
     case invalidParameterError
     case unknownError
-}
-
-extension WeatherError {
-    var errorMessage: String {
-        switch self {
-        case .invalidParameterError:
-            return "リクエストが不正です"
-        case .unknownError:
-            return "予期せぬエラーが発生しました"
-        }
-    }
 }
